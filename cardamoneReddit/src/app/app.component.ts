f752c9e8d69e91d2a4207c8485f6afe9bdb4dc74
Lich-senpai13 <papesenki.enrico@iisgalvanimi.edu.it>
2024-01-28 10:46:51 +0000

    Ciao

---
+++ cardamoneReddit/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'cardamoneReddit';
+  articles = new Array<string>(); //Dichiaro un array di stringhe
+  cliccaloStoCAZZOdi_bottone (newtitle: HTMLInputElement, span: HTMLIFrameElement): boolean
+  {
+    
+  }
+}
